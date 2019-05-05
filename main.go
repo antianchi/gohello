e73ef94db37ce8795741b9e258189ddc544a7e49
antch_hw <visual00>
2019-05-05 18:23:21 +0800

    beego 路由设置

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"github.com/astaxie/beego"
 	_ "gohello/routers"
+	"reflect"
 )
 
 func main() {
+
+	var (
+		a int
+		b int16
+		c = 'A'
+	)
+
+	fmt.Println(a, b)
+	fmt.Println(c)
+	fmt.Println(reflect.TypeOf(c))
 
 	// 静态文件处理，参数路径，文件夹路径
 	beego.SetStaticPath("/static1", "static")
