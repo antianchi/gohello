6bcaab3fcc2343d94801fcbcd85e6594ec959b99
antch2 <atc00@foxmail.com>
2019-05-06 00:00:21 +0800

    11

--- main.go
+++ main.go
@@ -12,12 +12,16 @@ func main() {
 	var (
 		a int
 		b int16
-		c = 'A'
+		c     = 'A'
+		d int = 65
 	)
 
+	e := string(d)
 	fmt.Println(a, b)
 	fmt.Println(c)
 	fmt.Println(reflect.TypeOf(c))
+
+	fmt.Println("e:" + e)
 
 	// 静态文件处理，参数路径，文件夹路径
 	beego.SetStaticPath("/static1", "static")
